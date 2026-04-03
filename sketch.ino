114bca9a8797df57f3dbfa7e66ac6e47683443be
jodonijr <jodonijr@hotmail.com>
2026-04-03 17:18:38 -0300

    correção: máscara radiação, chuva estava suprimindo decimais

--- sketch.ino
+++ sketch.ino
@@ -15,7 +15,7 @@ void loop() {
   Serial.printf("pressao: %.1f hPa (simulada)\n", dados.pressao);
   Serial.printf("orvalho: %.1f C\n", dados.pontoOrvalho);
   Serial.printf("vento: %.1f m/s\n", dados.velocidadeVento);
-  Serial.printf("radiacao: %1.f KJ/m2\n", dados.radiacaoSolar);
-  Serial.printf("chuva: %1.f mm\n", dados.chuvaAcumulada);
+  Serial.printf("radiacao: %.1f KJ/m2\n", dados.radiacaoSolar);
+  Serial.printf("chuva: %.1f mm\n", dados.chuvaAcumulada);
   delay(1000);
 }
